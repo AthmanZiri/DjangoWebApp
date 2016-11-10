a2503acef21cc442f4287638a10c06336e8403b0
athmanziri <issaziri@gmail.com>
2016-11-11 01:51:43 +0300

    blog working

---
+++ SiteBlog/models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.db import models
+
+class Post(models.Model):
+	title=models.CharField(max_length=140)
+	body=models.TextField(max_length=1500)
+	date=models.DateTimeField()
+
+	def __str__(self):
+		return self.title
+
+
